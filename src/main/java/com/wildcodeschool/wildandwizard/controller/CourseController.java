ff36d23b0b11e7bc4258d7d10b68833a11a9171a
EldorWildCodeSchool <eldor-rabe_student2021@wilder.school>
2021-11-13 19:32:29 +0100

    ...

--- src/main/java/com/wildcodeschool/wildandwizard/controller/CourseController.java
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/CourseController.java
@@ -1,10 +1,18 @@
 package com.wildcodeschool.wildandwizard.controller;
 
+import com.wildcodeschool.wildandwizard.entity.Course;
+import com.wildcodeschool.wildandwizard.entity.Wizard;
 import com.wildcodeschool.wildandwizard.repository.CourseRepository;
+import com.wildcodeschool.wildandwizard.repository.WizardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CourseController {
@@ -12,6 +20,8 @@ public class CourseController {
     @Autowired
     private CourseRepository courseRepository;
 
+    @Autowired
+    private WizardRepository wizardRepository;
 
     @GetMapping("/course")
     public String getCourses(Model out) {
@@ -20,6 +30,19 @@ public class CourseController {
         return "courses";
     }
 
+    @GetMapping("/students")
+        public String getStudents(Model out, @RequestParam(required = true, defaultValue = "0") Long idCourse){
 
+        Optional<Course> optionalCourse = courseRepository.findById(idCourse);
+        Course course = new Course();
+        List<Wizard> wizards = new ArrayList<>();
+        if (optionalCourse.isPresent()) {
+            course = optionalCourse.get();
+            wizards= course.getWizards();
+        }
+        out.addAttribute("course", course);
+        out.addAttribute("wizards", wizards);
 
+        return "students";
+        }
 }
